84475ff985f46ee40488871617b5f9b63ab4a877
Ahmadraza1123 <ahmadraza19397@gmail.com>
2025-08-31 09:24:19 +0500

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	api/views.py

--- DjangoProject2/settings.py
+++ DjangoProject2/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'api',
+    'blog',
     'rest_framework',
     'rest_framework.authtoken',
 ]
